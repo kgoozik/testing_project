b189c4666e2898be61f3c3c359a8c1ce24dc8eff
kgoozik <65785406+kgoozik@users.noreply.github.com>
2020-06-08 21:55:42 +0200

    Test1

--- todo-spec.js
+++ todo-spec.js
@@ -6,6 +6,30 @@ describe('angularjs homepage todo list', function() {
       element(by.model("first")).sendKeys("2");
       element(by.model("second")).sendKeys("2");
       element(by.buttonText("Go!")).click();
-      
+
+      element(by.binding("latest")).getText().then(function(text){
+        
+        console.log(text)
+
+      })
     });
+      
+    it('testing repeater_printing first row data', function() {
+        browser.get('https://juliemr.github.io/protractor-demo/');
+        
+        element(by.model("first")).sendKeys("2");
+        element(by.model("second")).sendKeys("2");
+        element(by.buttonText("Go!")).click();
+  
+        element(by.model("first")).sendKeys("5");
+        element(by.model("second")).sendKeys("10");
+        element(by.buttonText("Go!")).click();
+
+        console.log('testing repeater_printing first row data')
+        element(by.repeater("result in memory").row(0)).getText().then(function(text){
+
+        console.log(text)
+        });
+
+    })
   });
